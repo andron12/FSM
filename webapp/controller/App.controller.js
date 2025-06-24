ad47b17beaa35b98cffdb601f2ca393e6a62e9de
Andrii Markevyh <andrii_markevych@epam.com>
2025-06-24 12:04:14 +0300

    init2

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("epam.ui5extension.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
